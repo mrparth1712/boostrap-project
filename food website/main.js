572354c827180b2de06f3b69f77133ae54d07ad1
mrparth1712 <parthbajariya@gmail.com>
2025-02-21 10:49:27 +0530

    website

---
+++ food website/main.js
@@ -0,0 +1,30 @@
+//active navbar
+let nav=document.querySelector(".navigation-wrap");
+window.onscroll=function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+// counterdesign
+document.addEventListener("DOMContentLoaded",()=>{
+    function counter(id,start,end,duration){
+        let obj=document.getElementById (id),
+        current=start,
+        range=end-start,
+        increment=end>start?1:-1,
+        step=Math.abs(Math.floor(duration / range)),
+        timer=setInterval(()=>{
+            current+=increment;
+            obj.textContent=current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        },step);
+    }
+    counter("count1",0,300,3000);
+    counter("count2",100,550,2500);
+    counter("count3",0,640,3000);
+    counter("count4",0,800,3000);
+});
